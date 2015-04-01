ef3492eeb3ff17dae73121952eec9851e4da4487
Takahiko Kawasaki <takahiko.kawasaki@neovisionaries.com>
2015-04-01 18:23:51 +0900

    Started to implement WebSocketInputStream.readFrame().

--- src/main/java/com/neovisionaries/ws/client/WebSocketInputStream.java
+++ src/main/java/com/neovisionaries/ws/client/WebSocketInputStream.java
@@ -17,6 +17,7 @@ package com.neovisionaries.ws.client;
 
 
 import java.io.ByteArrayOutputStream;
+import java.io.EOFException;
 import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -96,5 +97,83 @@ class WebSocketInputStream extends FilterInputStream
 
         // Convert the byte array to a string.
         return baos.toString("UTF-8");
+    }
+
+
+    public WebSocketFrame readFrame() throws IOException
+    {
+        // Buffer.
+        byte[] buffer = new byte[8];
+
+        // Read the first two bytes.
+        readBytes(buffer, 2);
+
+        // FIN
+        boolean fin = ((buffer[0] & 0x80) != 0);
+
+        // RSV1, RSV2, RSV3
+        boolean rsv1 = ((buffer[0] & 0x40) != 0);
+        boolean rsv2 = ((buffer[0] & 0x20) != 0);
+        boolean rsv3 = ((buffer[0] & 0x10) != 0);
+
+        // Opcode
+        int opcode = (buffer[0] & 0x0F);
+
+        // Masked
+        boolean masked = ((buffer[1] & 0x80) != 0);
+
+        // The payload length. It is expressed in 7 bits.
+        long payloadLength = buffer[1] & 0x7F;
+
+        if (payloadLength == 126)
+        {
+            // Read the extended payload length.
+            // It is expressed in 2 bytes in network byte order.
+            readBytes(buffer, 2);
+
+            // Interpret the bytes as a number.
+            payloadLength = (((buffer[0] & 0xFF) << 8) |
+                             ((buffer[1] & 0xFF)     ));
+        }
+        else if (payloadLength == 127)
+        {
+            // Read the extended payload length.
+            // It is expressed in 8 bytes in network byte order.
+            readBytes(buffer, 8);
+
+            // Interpret the bytes as a number.
+            payloadLength = (((buffer[0] & 0xFF) << 56) |
+                             ((buffer[1] & 0xFF) << 48) |
+                             ((buffer[2] & 0xFF) << 40) |
+                             ((buffer[3] & 0xFF) << 32) |
+                             ((buffer[4] & 0xFF) << 24) |
+                             ((buffer[5] & 0xFF) << 16) |
+                             ((buffer[6] & 0xFF) <<  8) |
+                             ((buffer[7] & 0xFF)      ));
+        }
+
+        // Masking-key
+        byte[] mask = null;
+
+        if (masked)
+        {
+            mask = new byte[4];
+            readBytes(mask, 4);
+        }
+
+        // TODO
+        return null;
+    }
+
+
+    private void readBytes(byte[] buffer, int length) throws IOException
+    {
+        // Read
+        int count = super.read(buffer, 0, length);
+
+        if (count != length)
+        {
+            throw new EOFException("The end of the stream was reached unexpectedly.");
+        }
     }
 }
